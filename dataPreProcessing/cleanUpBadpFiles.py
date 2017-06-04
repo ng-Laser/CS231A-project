b76a5daebf76bb9138af63001677d7262e6f354d
Noa Glaser <noa.glaser@gmail.com>
2017-06-04 02:38:41 +0000

    tiny helper to get rid of old buggy .p files

---
+++ dataPreProcessing/cleanUpBadpFiles.py
@@ -0,0 +1,27 @@
+import sys 
+import os
+import pickle
+
+def forAllFilesInDir(dr):
+    numDeleted = 0
+    for f in os.listdir(dr):
+      if not f.endswith('.p'):
+         continue
+      fileName = os.path.join(dr, f) 
+      a = pickle.load(open(fileName, 'rb'))
+      if(a['data'].shape[0] != a['frames'].shape[0]):
+        print('Deleting {0}'.format(fileName))
+        os.system('rm ' +  fileName)
+        numDeleted = numDeleted + 1
+    print('Num deleted {0}'.format(numDeleted))
+ 
+
+if __name__ == '__main__': 
+  if len(sys.argv) != 2:
+      print(
+       "Expecting 1 arguments:\n" + 
+        "Path to directory with .p files"
+      )
+      exit()
+  sourceFile = sys.argv[1]
+  forAllFilesInDir(sourceFile)
